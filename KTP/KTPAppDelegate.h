b5f0734afce487f594a3fe32ddff0f017675aabe
Greg Azevedo <gazevedo@umich.edu>
2014-08-13 01:17:00 -0400

    initial commit

---
+++ KTP/KTPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KTPAppDelegate.h
+//  KTP
+//
+//  Created by Greg Azevedo on 8/13/14.
+//  Copyright (c) 2014 Kappa Theta Pi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KTPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KTP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KTP
+//
+//  Created by Greg Azevedo on 8/13/14.
+//  Copyright (c) 2014 Kappa Theta Pi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KTPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KTPAppDelegate class]));
+    }
+}
