cbc5afee2671341c9a32469f7e6d5f5681f6d6e7
owen-yang <owenyang@umich.edu>
2015-02-07 14:35:54 -0500

    Added KTPPledgeTaskViewController

---
+++ KTP/KTPPledgeTaskViewController.h
@@ -0,0 +1,16 @@
+//
+//  KTPPledgeTaskViewController.h
+//  KTP
+//
+//  Created by Owen Yang on 2/7/15.
+//  Copyright (c) 2015 Kappa Theta Pi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class KTPPledgeTask;
+
+@interface KTPPledgeTaskViewController : UIViewController
+
+@property (nonatomic, strong) KTPPledgeTask *pledgeTask;
+
+@end
