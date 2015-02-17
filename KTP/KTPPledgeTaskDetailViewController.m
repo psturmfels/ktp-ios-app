20897dcc9731501802d06750ef0df51fa18071aa
Patrick Wilson <pdubslax@umich.edu>
2015-02-16 22:20:19 -0500

    fixed duplicate files

--- KTP/KTPPledgeTaskDetailViewController.m
+++ KTP/KTPPledgeTaskDetailViewController.m
@@ -7,10 +7,16 @@
 //
 
 #import "KTPPledgeTaskDetailViewController.h"
+#import "KTPPledgeTask.h"
+
+
 
 @interface KTPPledgeTaskDetailViewController ()
 
 @end
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIView *contentView;
 
 @implementation KTPPledgeTaskDetailViewController
 
@@ -43,7 +49,40 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self loadSubviews];
+    [self autoLayoutSubviews];
 }
 
+- (void)loadSubviews {
+    [self loadScrollView];
+    [self loadContentView];
+    
+    // IMPLEMENT
+}
 
+- (void)loadScrollView {
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
+    self.scrollView.alwaysBounceVertical = YES;
+    [self.view addSubview:self.scrollView];
+}
+
+- (void)loadContentView {
+    self.contentView = [[UIView alloc] initWithFrame:self.view.frame];
+    [self.scrollView addSubview:self.contentView];
+}
+
+- (void)autoLayoutSubviews {
+    for (UIView *view in self.contentView.subviews) {
+        view.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+    
+    // IMPLEMENT
+    
+    NSDictionary *views = @{
+                            
+                            };
+    
+    
+}
 @end
