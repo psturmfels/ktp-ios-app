cab2f95bf5e86c1c2cb2f597858b33ebf0fe205a
chuckry <chuckry.vengadam@gmail.com>
2015-02-18 21:26:18 -0500

    Laid out meetings cell for actives and pledges

--- KTP/KTPPledgeMeetingsCell.swift
+++ KTP/KTPPledgeMeetingsCell.swift
@@ -7,4 +7,53 @@
 //
 
 import Foundation
+import UIKit
+
+
+class KTPPledgeMeetingsCell : UITableViewCell {
+
+    var meeting : KTPPledgeMeeting? {
+        didSet {
+            loadLabelValues();
+        }
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .Default, reuseIdentifier: reuseIdentifier);
+    }
+
+    convenience required init(coder aDecoder: NSCoder) {
+        self.init(style: .Default, reuseIdentifier: "");
+    }
+    
+    func loadLabelValues() {
+        
+        // Load a list of pledges or actives depending on current user:
+        
+        if (KTPSUser.currentUser().member.status == "Pledge") {
+            self.textLabel!.text = meeting!.active.firstName + " " + meeting!.active.lastName;
+        }
+        
+        else {
+            self.textLabel!.text = meeting!.pledge.firstName + " " + meeting!.pledge.lastName;
+        }
+        
+        // Load some sort of boolean indicator for "have they met"
+        
+        if (meeting!.complete) {
+            accessoryType = .Checkmark;
+        } else {
+            accessoryType = .None;
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
 
