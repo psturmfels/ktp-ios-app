bd03894a316413e00eca62b5d0a3c742576ddc32
Owen Yang <yoyoyango123@gmail.com>
2015-02-26 02:30:41 -0500

    Removed KTPSPledgeMeetings, added KTPPledgeOverviewViewController

---
+++ KTP/KTPPledgeOverviewViewController.m
@@ -0,0 +1,23 @@
+//
+//  KTPPledgeOverviewViewController.m
+//  KTP
+//
+//  Created by Owen Yang on 2/26/15.
+//  Copyright (c) 2015 Kappa Theta Pi. All rights reserved.
+//
+
+#import "KTPPledgeOverviewViewController.h"
+
+@interface KTPPledgeOverviewViewController ()
+
+@end
+
+@implementation KTPPledgeOverviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
